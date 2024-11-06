6cc5a0f81b65f809acd8bb65cf0dc8758ce5ef26
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2024-11-05 21:19:48 -0300

    :arrow_up: chore: Install new dependecies: cors and bcrypt

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import fastify from 'fastify';
+import { routes } from 'web/routes/routes';
+import cors from '@fastify/cors';
 
 const app = fastify();
+
+app.register(cors, {
+  origin: '*',
+});
+
+app.register(routes);
 
 export { app };
