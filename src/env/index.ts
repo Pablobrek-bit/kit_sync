aeb7e23e3ee603d063fac20270d5e8ed13bdecc6
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2024-11-10 08:21:09 -0300

    📝 docs: added security key to environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,6 +6,7 @@ config();
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
+  SECRET_KEY: z.string().default('secret'),
 });
 
 const _env = envSchema.safeParse(process.env);
