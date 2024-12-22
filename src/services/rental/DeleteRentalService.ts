fa8954d421d73e3fee67751a1e5de6d59d0958dc
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2024-12-22 08:52:20 -0300

    ✨ feat: modified function delete in the rental

--- src/services/rental/DeleteRentalService.ts
+++ src/services/rental/DeleteRentalService.ts
@@ -1,14 +1,16 @@
+import { RentalStatus } from '@prisma/client';
 import type { RentalRepository } from 'repository/interfaces/RentalRepository';
 
 interface DeleteRentalServiceRequest {
   rentalId: string;
   userId: string;
+  status: RentalStatus;
 }
 
 export class DeleteRentalService {
   constructor(private rentalRepository: RentalRepository) {}
 
-  async execute({ rentalId, userId }: DeleteRentalServiceRequest) {
+  async execute({ rentalId, userId, status }: DeleteRentalServiceRequest) {
     const rental = await this.rentalRepository.findById(rentalId);
 
     if (!rental) {
@@ -19,6 +21,19 @@ export class DeleteRentalService {
       throw new Error('You are not allowed to delete this rental');
     }
 
-    await this.rentalRepository.delete(rentalId);
+    if (
+      rental.status != RentalStatus.FINISHED &&
+      rental.status != RentalStatus.CANCELLED
+    ) {
+      throw new Error(
+        'You can only delete a rental with status FINISHED or CANCELLED',
+      );
+    }
+
+    await this.rentalRepository.update({
+      id: rentalId,
+      status,
+      deleteAt: new Date(),
+    });
   }
 }
