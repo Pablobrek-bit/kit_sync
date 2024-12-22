9918f5a605aeb339f0388e94c27434274720606d
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2024-12-22 20:16:47 -0300

    🔥 chore: removed deprecated commentary

--- src/web/routes/equipamentRoutes.ts
+++ src/web/routes/equipamentRoutes.ts
@@ -5,11 +5,6 @@ import { EquipamentController } from 'web/controllers/EquipamentController';
 export async function equipamentRoutes(app: FastifyInstance) {
   const equipamentController = new EquipamentController();
 
-  // post /equipament x
-  // get /equipament/{equipamentId} x
-  // put /equipament/{equipamentId}
-  // delete /equipament/{equipamentId} x
-  // get /equipment/search
   app.addHook('preHandler', ensureAuthenticated);
 
   app.post('/', equipamentController.create);
