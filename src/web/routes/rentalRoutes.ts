16af790786d0a1741e9e1e9fe1de424921b47441
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2024-12-23 11:10:56 -0300

    🔥 chore: commentary removed

--- src/web/routes/rentalRoutes.ts
+++ src/web/routes/rentalRoutes.ts
@@ -3,12 +3,6 @@ import { ensureAuthenticated } from 'middleware/ensureAuthenticated';
 import { RentalController } from 'web/controllers/RentalController';
 
 export async function rentalRoutes(app: FastifyInstance) {
-  // POST /equipment: Cadastrar um novo equipamento.
-  // GET /equipment/{equipmentId}: Obter informações de um equipamento específico.
-  // PUT /equipment/{equipmentId}: Atualizar informações de um equipamento.
-  // DELETE /equipment/{equipmentId}: Deletar um equipamento.
-  // GET /equipment/search: Buscar equipamentos com filtros (tipo, localização, preço, disponibilidade, etc.)
-
   const rentalController = new RentalController();
 
   app.addHook('preHandler', ensureAuthenticated);
