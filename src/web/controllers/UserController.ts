48fcd6e87911e5cc5c1dd81377113ab8fe1a0f7e
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-01-17 20:04:00 -0300

    ✨ feat: added new verification in the create user schema

--- src/web/controllers/UserController.ts
+++ src/web/controllers/UserController.ts
@@ -11,16 +11,19 @@ import { z } from 'zod';
 export class UserController {
   async create(request: FastifyRequest, reply: FastifyReply) {
     const requestSchema = z.object({
-      name: z.string({
-        required_error: 'Name is required',
-        invalid_type_error: 'Name must be a string',
-      }),
+      name: z
+        .string({
+          required_error: 'Name is required',
+          invalid_type_error: 'Name must be a string',
+        })
+        .min(3, { message: 'Name must have at least 3 characters' }),
       email: z
         .string({
           required_error: 'Email is required',
           invalid_type_error: 'Email must be a string',
         })
-        .email({ message: 'Invalid email' }),
+        .email({ message: 'Invalid email' })
+        .min(3, { message: 'Email must have at least 3 characters' }),
       password: z
         .string({
           required_error: 'Password is required',
