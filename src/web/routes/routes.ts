05ca5b224d13009ab3f3e2deb007a977074f2bc4
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-01-26 09:53:11 -0300

    ✨ feat: added messages route

--- src/web/routes/routes.ts
+++ src/web/routes/routes.ts
@@ -4,6 +4,7 @@ import { authRoutes } from './authRoutes';
 import { rentalRoutes } from './rentalRoutes';
 import { equipamentRoutes } from './equipamentRoutes';
 import { reviewRoutes } from './reviewRoutes';
+import { messageRoutes } from './messageRoutes';
 
 export async function routes(app: FastifyInstance) {
   app.register(userRoutes, { prefix: '/users' });
@@ -11,4 +12,5 @@ export async function routes(app: FastifyInstance) {
   app.register(equipamentRoutes, { prefix: '/equipments' });
   app.register(rentalRoutes, { prefix: '/rentals' });
   app.register(reviewRoutes, { prefix: '/reviews' });
+  app.register(messageRoutes, { prefix: '/messages' });
 }
